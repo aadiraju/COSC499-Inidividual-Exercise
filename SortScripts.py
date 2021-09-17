383d209fd8410bd0ebba2883ba130af09b831ca4
Abhineeth Adiraju <abhineeth00@gmail.com>
2021-09-16 21:06:29 -0700

    Added functionality to sort 1D lists and updated README.md

---
+++ SortScripts.py
@@ -0,0 +1,25 @@
+import random
+
+
+def sort_int_list(array):
+    '''
+    :param array:
+    returns a sorted version of array using the bubble sort method in ascending order
+    '''
+    arr = array.copy()
+    size = len(arr)
+    for i in range(size - 1):
+        for j in range(size - i - 1):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+
+    return arr
+
+
+# Run with sample values
+size = random.randint(2, 10)
+arr = []
+for i in range(size):
+    arr.append(random.randint(1, 10000))
+print('Unsorted Array: {0}'.format(arr))
+print('Sorted Array: {}'.format(sort_int_list(arr)))
